a67d8579b1d6becc4b81bb26eee61c79c271761a
Masum8823 <masum688823@gmail.com>
2026-05-05 00:16:27 +0600

    Add StudySession model with encapsulated user activity data

---
+++ src/burnoutdetector/StudySession.java
@@ -0,0 +1,38 @@
+package burnoutdetector;
+
+public class StudySession {
+    private String date;
+    private double stu, scr, hobb, wst, slp, walk;
+    private int f, d, a, m, i;
+
+    public StudySession(String date, double stu, double scr, double hobb, double wst, double slp, double walk, int f, int d, int a, int m, int i) {
+        this.date = date; 
+        this.stu = stu; 
+        this.scr = scr;
+        this.hobb = hobb; 
+        this.wst = wst;
+        this.slp = slp;
+        this.walk = walk;
+        this.f = f; this.d = d; this.a = a; this.m = m; this.i = i;
+    }
+
+    public double getNetScore() {
+        double score = (stu * 10) + (scr * -15) + (hobb * 8) + (walk * 12) + (wst * -5) + (slp * 5);
+        int prayerScore = (f + d + a + m + i) * 5;
+        return score + prayerScore;
+        }
+
+    public int getPrayerCount() { 
+        return f + d + a + m + i; 
+        }
+
+    public String getDate() {
+         return date; 
+        }
+
+    @Override
+    public String toString() {
+        // Index mapping: 0:Name, 1:Date, 2:Stu, 3:Scr, 4:Hobb, 5:Wst, 6:Slp, 7:Walk, 8-12:Prayers, 13:TotalScore
+        return date + "," + stu + "," + scr + "," + hobb + "," + wst + "," + slp + "," + walk + "," + f + "," + d + "," + a + "," + m + "," + i + "," + getNetScore();
+    }
+}

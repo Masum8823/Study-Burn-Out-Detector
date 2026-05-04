341ccd23d9131727296ac18e7f7a2e858f8a8457
Masum8823 <masum688823@gmail.com>
2026-05-04 22:19:59 +0600

    Implement StudyActivity class with overridden calculatePoints logic

---
+++ src/burnoutdetector/StudyActivity.java
@@ -0,0 +1,6 @@
+package burnoutdetector;
+public class StudyActivity extends Activity {
+    public StudyActivity(double h) { super(h); }
+    @Override
+    public double calculatePoints() { return hours * 10; } 
+}
